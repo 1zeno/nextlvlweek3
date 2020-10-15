01a71fb12a3fa4e386ea3c90614cc7a5e27eaab8
Emanuel Souza <emanuel-souza1@hotmail.com>
2020-10-15 20:32:45 -0300

    landing page done and orphanagesmap started

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,11 @@ import React from "react";
 import "./styles/global.css";
 import "./styles/pages/landing.css";
 
-import logoImg from "./images/logo.svg";
+import Routes from "./routes";
 
 function App() {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p> Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-
-        <div className="location">
-          <strong>Lauro de Freitas</strong>
-          <span>Bahia</span>
-        </div>
-
-        <a href="#" className="enter-app">
-          link
-        </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
