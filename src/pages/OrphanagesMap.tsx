bd3ee636dc65f6847c22dbc60dc6862ad6206a44
Emanuel Souza <emanuel-souza1@hotmail.com>
2020-10-15 23:45:57 -0300

    episode 1 finished

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,9 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FiPlus } from "react-icons/fi";
+import { Map, TileLayer } from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+
+import "../styles/pages/orphanages-map.css";
+
+import mapMarkerImg from "../images/map-marker.svg";
 
 function OrphanagesMap(){
 
     return(
-        <h1>SALVE SALVE</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy"></img>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+
+                <footer>
+                    <strong>Lauro de Freitas</strong>
+                    <span>Bahia</span>
+                </footer>
+            </aside>
+
+            <Map
+                center={[-12.8981605,-38.2849455]}
+                zoom={15}
+                style={{ width:"100%", height: "100%" }}>
+                    <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>
+            </Map>
+
+            <Link to={""} className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     );
 }
 
